e70e7d281a2ae1169541fbc45e84356fa785f7fc
your name here <theo@dali.dartmouth.edu>
2018-11-15 15:15:16 -0800

    Got all login functionality working

--- KiwiMusic/app/controllers/sessions_controller.rb
+++ KiwiMusic/app/controllers/sessions_controller.rb
@@ -1,24 +1,21 @@
 class SessionsController < ApplicationController
 
   def new
-    render :new
-    #logic that would say if you are logged in already to to somewhere
   end
 
   def create
     @user = User.find_by(email: params[:user][:email])
-    if @user && BCrypt::Password.new(user.password_digest).is_password?(params[:user][:password])
+    if @user && BCrypt::Password.new(@user.password_digest).is_password?(params[:user][:password])
       login!(@user)
-      redirect_to user_url(@user.id)
+      redirect_to user_url(@user)
     else
       flash.now[:error] = ['invalid credentials']
     end
   end
 
   def destroy
-    @user.session_token = nil
     session[:session_token] = nil
+    render :new
   end
-
 
 end
--- KiwiMusic/app/models/user.rb
+++ KiwiMusic/app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
 
   def reset_session_token!
     self.session_token = User.generate_session_token
-    self.save!
+    self.save
     self.session_token
   end
 
