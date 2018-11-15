2082a6ac5d454bbf525bf66d79b7192911fa36d6
your name here <theo@dali.dartmouth.edu>
2018-11-15 10:26:36 -0800

    Create user model to handle session tokens and password security

--- KiwiMusic/app/models/user.rb
+++ KiwiMusic/app/models/user.rb
@@ -1,3 +1,35 @@
 class User < ApplicationRecord
 
+  after_initialize { ensure_session_token }
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= reset_session_token!
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.is_password?(password)
+  end
+
+  def find_by_credentials(email, password)
+    user = Users.find_by(email: :email)
+    return user if user && user.is_password?(password)
+    return nil
+  end
+
+
 end
